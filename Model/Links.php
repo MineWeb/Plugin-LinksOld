64ab0ef0d3e783f2958d7089184f54cac34d9b66
nivcoo <nico64800@live.fr>
2019-02-03 16:49:47 +0100

    Fix. bug model

--- Model/Links.php
+++ Model/Links.php
@@ -1,24 +1,26 @@
 <?php
 class Links extends LinksAppModel
 {
-    public function edit($id, $type, $title, $subtitle, $link, $discord_id)
+    public function edit($id, $type, $title, $subtitle, $link, $discord_id, $discord_channel)
     {
-		$type = $this->getDataSource()->value($type, 'string');
-		$title = $this->getDataSource()->value($title, 'string');
-		$subtitle = $this->getDataSource()->value($subtitle, 'string');
-		$link = $this->getDataSource()->value($link, 'string');
-		$discord_id = $this->getDataSource()->value($discord_id, 'string');
-		
-		return $this->updateAll([
-			'type' => $type,
-			'title' => $title,
-			'subtitle' => $subtitle,
-			'link' => $link,
-			'discord_id' => $discord_id
-		], ['id' => $id]);
-	}
-    
-    public function add($type, $title, $subtitle, $link, $discord_id)
+        $type = $this->getDataSource()->value($type, 'string');
+        $title = $this->getDataSource()->value($title, 'string');
+        $subtitle = $this->getDataSource()->value($subtitle, 'string');
+        $link = $this->getDataSource()->value($link, 'string');
+        $discord_id = $this->getDataSource()->value($discord_id, 'string');
+        $discord_id = $this->getDataSource()->value($discord_id, 'string');
+
+        return $this->updateAll([
+            'type' => $type,
+            'title' => $title,
+            'subtitle' => $subtitle,
+            'link' => $link,
+            'discord_id' => $discord_id,
+            'discord_channel' => $discord_channel
+        ], ['id' => $id]);
+    }
+
+    public function add($type, $title, $subtitle, $link, $discord_id, $discord_channel)
     {
         $this->create();
         $this->set(array(
@@ -26,8 +28,9 @@ class Links extends LinksAppModel
             'title' => $title,
             'subtitle' => $subtitle,
             'link' => $link,
-            'discord_id' => $discord_id
+            'discord_id' => $discord_id,
+            'discord_channel' => $discord_channel
         ));
         $this->save();
-	}
+    }
 }
